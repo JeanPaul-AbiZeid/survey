189f82a290ce31f0e02c100220f21d3c4501291d
jean paul <jeanpaulabizeid@gmail.com>
2022-06-17 11:33:07 +0300

    creating an api to get a single or all surveys

---
+++ survey-laravel/app/Http/Controllers/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Survey;
+
+class UserController extends Controller
+{
+    public function getAllsurveys($id = null){
+        if($id != null){
+            $survey = Survey::find($id);
+        }else{
+            $survey = Survey::all();
+        }
+        
+        return response()->json([
+            "status" => "Success",
+            "surveys" => $survey
+        ], 200);
+    }
+}
--- survey-laravel/routes/api.php
+++ survey-laravel/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
@@ -15,4 +16,9 @@ Route::controller(AuthController::class)->group(function () {
 
 Route::controller(AdminController::class)->group(function () {
     Route::get('/gettypes', 'getAlltypes');
+});
+
+Route::controller(UserController::class)->group(function () {
+    Route::get('/getsurveys/{id?}', 'getAllsurveys');
+
 });
