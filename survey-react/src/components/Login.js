c34abf11ff1d8601514e0118759bc90fb41fe923
jean paul <jeanpaulabizeid@gmail.com>
2022-06-18 15:39:14 +0300

    styling the log in page

--- survey-react/src/components/Login.js
+++ survey-react/src/components/Login.js
@@ -5,19 +5,22 @@ import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
-        <h1>Sign In</h1>
-        <form>
-            <input type="text" placeholder="Email address" id="email"/><br/>
-            <input type="password" placeholder="Password" id="password"/>
-            <div id="pass">
-                <input type="checkbox" id="toggle"/>
-                Show Password
-            </div>
-            <input type="submit" id = "login" class="btn btn-black" value="Log In"/>
-            <h4>New account? <Link to="/Signup">Sign up</Link></h4>
-        </form>
+    <div className="main">
+        <div className="form-container">
+            <h1>Sign In</h1>
+            <form id="form">
+                <input type="text" placeholder="Email address" id="email"/><br/>
+                <input type="password" placeholder="Password" id="password"/>
+                <div id="pass">
+                    <input type="checkbox" id="toggle"/>
+                    Show Password
+                </div>
+                <input type="submit" id = "login" class="btn btn-gray" value="Log In"/>
+                <h4>New account? <Link to="/Signup">Sign up</Link></h4>
+            </form>
+        </div>
     </div>
+    
   );
 };
 
