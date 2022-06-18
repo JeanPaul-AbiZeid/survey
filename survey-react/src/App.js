1bff4f5ffd1ea131c2593ce77c1540b214878e4f
jean paul <jeanpaulabizeid@gmail.com>
2022-06-18 15:06:23 +0300

    creating the Log in page

--- survey-react/src/App.js
+++ survey-react/src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './components/Login'
 
 function App() {
   return (
-    <>
-    
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={
+          <>
+          <Login />
+        </>
+
+        }></Route>
+      </Routes>
+      
+    </BrowserRouter>
   );
 }
 
---
+++ survey-react/src/components/Login.js
@@ -0,0 +1,23 @@
+import React from "react";
+import '../App.css';
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <div>
+        <h1>Sign In</h1>
+        <form>
+            <input type="text" placeholder="Email address" id="email"/><br/>
+            <input type="password" placeholder="Password" id="password"/>
+            <div id="pass">
+                <input type="checkbox" id="toggle"/>
+                Show Password
+            </div>
+            <input type="submit" id = "login" class="btn btn-black" value="Log In"/>
+            <h4>New account? <Link to="/Signup">Sign up</Link></h4>
+        </form>
+    </div>
+  );
+};
+
+export default Login;
