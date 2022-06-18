c61f5ac60495fb67a560160a3232b92b771493ee
jean paul <jeanpaulabizeid@gmail.com>
2022-06-18 21:24:56 +0300

    adding nav bar to the surveys page

--- survey-react/src/components/Surveys.js
+++ survey-react/src/components/Surveys.js
@@ -1,9 +1,28 @@
 import React from "react";
 import '../App.css';
+import Button from "./Button";
+import { Link, useNavigate } from "react-router-dom";
 
 const Surveys = () => {
+    const Navigation = useNavigate();
     return (
-      <h2>test</h2>
+      <div>
+        <nav className="top-nav">
+            <h1>S-Maker</h1>
+            <ul className="links">
+                <li>About Us</li>
+                <li>Contact</li>
+                <li>
+                    <button className="btn-white" onClick={
+                        function logout(){
+                            localStorage.removeItem("jwt");
+                            Navigation("/")
+                        }
+                    }>Log Out</button>
+                </li>
+            </ul>
+        </nav>
+      </div>
     );
   };
 
