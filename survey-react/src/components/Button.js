11cf604bab2dbe005ff5dd993a1dda1907f154a0
jean paul <jeanpaulabizeid@gmail.com>
2022-06-18 16:17:32 +0300

    adding the button component + nav bar to the sign up page

---
+++ survey-react/src/components/Button.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Button = ({text }) => {
+  return (
+    <button
+      className="btn-white"
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button;
--- survey-react/src/components/Signup.js
+++ survey-react/src/components/Signup.js
@@ -1,22 +1,36 @@
 import React from "react";
 import '../App.css';
 import { Link } from "react-router-dom";
+import Button from "./Button";
 
 const Signup = () => {
   return (
-    <div className="main">
-        <div className="form-container">
-            <h1>New Account</h1>
-            <form id="form">
-                <input type="text" placeholder="First Name" id="first_name"/><br/>
-                <input type="text" placeholder="Last Name" id="last_name"/><br/>
-                <input type="text" placeholder="Email address" id="email"/><br/>
-                <input type="password" placeholder="Password" id="password"/>
-                <div id="pass"><input type="checkbox" id="toggle"/>Show Password</div>
-                <input type="submit" id = "sign_up" className="btn btn-gray" value="Sign Up"/>
-            </form>
+    <div>
+        <nav className="top-nav">
+            <h1>S-Maker</h1>
+            <ul className="links">
+                <li>About Us</li>
+                <li>Contact</li>
+                <li>
+                    <Link to="/"><Button text={"Log In"}/></Link>
+                </li>
+            </ul>
+        </nav>
+        <div className="main">
+            <div className="form-container">
+                <h1>New Account</h1>
+                <form id="form">
+                    <input type="text" placeholder="First Name" id="first_name"/><br/>
+                    <input type="text" placeholder="Last Name" id="last_name"/><br/>
+                    <input type="text" placeholder="Email address" id="email"/><br/>
+                    <input type="password" placeholder="Password" id="password"/>
+                    <div id="pass"><input type="checkbox" id="toggle"/>Show Password</div>
+                    <input type="submit" id = "sign_up" className="btn btn-gray" value="Sign Up"/>
+                </form>
+            </div>
         </div>
     </div>
+    
     
   );
 };
