00fea21cc6e6aee8840ba9e4c3cf43e8f3f1fcb0
jean paul <jeanpaulabizeid@gmail.com>
2022-06-18 15:57:53 +0300

    adding a nav bar to the log in page

--- survey-react/src/components/Login.js
+++ survey-react/src/components/Login.js
@@ -5,21 +5,31 @@ import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div className="main">
-        <div className="form-container">
-            <h1>Sign In</h1>
-            <form id="form">
-                <input type="text" placeholder="Email address" id="email"/><br/>
-                <input type="password" placeholder="Password" id="password"/>
-                <div id="pass">
-                    <input type="checkbox" id="toggle"/>
-                    Show Password
-                </div>
-                <input type="submit" id = "login" class="btn btn-gray" value="Log In"/>
-                <h4>New account? <Link to="/Signup">Sign up</Link></h4>
-            </form>
+    <div>
+        <nav className="top-nav">
+            <h1>S-Maker</h1>
+            <ul className="links">
+                <li>About Us</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+        <div className="main">
+            <div className="form-container">
+                <h1>Sign In</h1>
+                <form id="form">
+                    <input type="text" placeholder="Email address" id="email"/><br/>
+                    <input type="password" placeholder="Password" id="password"/>
+                    <div id="pass">
+                        <input type="checkbox" id="toggle"/>
+                        Show Password
+                    </div>
+                    <input type="submit" id = "login" className="btn btn-gray" value="Log In"/>
+                    <h4>New account? <Link to="/Signup">Sign up</Link></h4>
+                </form>
+            </div>
         </div>
     </div>
+    
     
   );
 };
