277c7d4c9bf8c6d279b4bb71e81abca11113e377
jean paul <jeanpaulabizeid@gmail.com>
2022-06-18 20:46:33 +0300

    implementing axios to log in

--- survey-react/src/components/Login.js
+++ survey-react/src/components/Login.js
@@ -1,9 +1,16 @@
 import React from "react";
 import '../App.css';
 import Signup from './Signup'
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from 'axios';
+
+
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const Navigation = useNavigate();
   return (
     <div>
         <nav className="top-nav">
@@ -17,13 +24,44 @@ const Login = () => {
             <div className="form-container">
                 <h1>Sign In</h1>
                 <form id="form">
-                    <input type="text" placeholder="Email address" id="email"/><br/>
-                    <input type="password" placeholder="Password" id="password"/>
+                    <input type="text" placeholder="Email address" id="email" onChange={(e) => {setEmail(e.target.value);}}/><br/>
+                    <input type="password" placeholder="Password" id="password" onChange={(e) => {setPassword(e.target.value);}}/>
                     <div id="pass">
                         <input type="checkbox" id="toggle"/>
                         Show Password
                     </div>
-                    <input type="submit" id = "login" className="btn btn-gray" value="Log In"/>
+                    <button type="submit" className="btn btn-gray" onClick={
+                        function onSubmit(e){
+                        e.preventDefault();
+                        
+                        let data = new FormData();
+  
+                        data.append('email', email);
+                        data.append('password', password);
+                        
+                        axios({
+                            method: 'post',
+                            url: 'http://127.0.0.1:8000/api/login',
+                            data: data,
+                          })
+                          .then(function (response) {
+                            var token = response.data.authorisation.token
+                            localStorage.setItem("jwt", token);
+                            Navigation("/Signup")
+                            console.log(response)
+                            
+                          })
+                          .catch(function (error){
+                              if(error.response.data.message == "Unauthorized"){
+                                  alert("Incorrect email or password");
+                              }
+                              else{
+                                  alert(error.response.data.message);
+                              }
+                          })
+                        }}
+                    >Log in</button>
+                    
                     <h4>New account? <Link to="/Signup">Sign up</Link></h4>
                 </form>
             </div>
