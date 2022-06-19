190c0a61a55f706e143fb6e10ea73f174141864c
jean paul <jeanpaulabizeid@gmail.com>
2022-06-19 12:40:02 +0300

    creating a dropdown list component to select a type

--- survey-react/src/components/Admin.js
+++ survey-react/src/components/Admin.js
@@ -1,6 +1,7 @@
 import React from "react";
 import '../App.css';
 import Button from "./Button";
+import Types from "./Types";
 import { Link, useNavigate } from "react-router-dom";
 
 const Admin = () => {
@@ -23,6 +24,7 @@ const Admin = () => {
             </ul>
         </nav>
         
+        <Types />
         
       </div>
     );
---
+++ survey-react/src/components/Types.js
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
+
+const Types = ({text }) => {
+  
+  const [types, setTypes] = useState([]);
+
+  const fetchTypes = async () => {
+      try {
+          const res = await fetch("http://127.0.0.1:8000/api/gettypes");
+          const data = await res.json();
+          return data;
+      } catch (err) {
+          console.log(err);
+      }
+  };
+  
+  useEffect(() => {
+  //Accepts a function to perform on certain changes
+  const getTypes = async () => {
+      const serverTypes = await fetchTypes();
+      setTypes(serverTypes.types);
+  };
+  getTypes();
+  }, []);
+  
+
+  return (
+    <select id="types">
+      {types.map((type) => (
+        <option key={type.id}> {type.type}</option>
+      ))}
+    </select>
+  );
+};
+
+export default Types;
