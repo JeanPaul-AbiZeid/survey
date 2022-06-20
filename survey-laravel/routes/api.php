0d179ffc2c9f37546cc8e9e73fe0695630e0a847
jean paul <jeanpaulabizeid@gmail.com>
2022-06-20 09:07:03 +0300

    implementing admin middleware

--- survey-laravel/routes/api.php
+++ survey-laravel/routes/api.php
@@ -13,12 +13,13 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('refresh', 'refresh');
 
 });
-
-Route::controller(AdminController::class)->group(function () {
-    Route::get('/gettypes', 'getAlltypes');
-    Route::post('/addsurvey', 'addSurvey');
-    Route::post('/addquestion', 'addQuestion');
-    Route::post('/addoption', 'addOption');
+Route::group(['middleware' => 'role.admin'], function() {
+    Route::controller(AdminController::class)->group(function () {
+        Route::get('/gettypes', 'getAlltypes');
+        Route::post('/addsurvey', 'addSurvey');
+        Route::post('/addquestion', 'addQuestion');
+        Route::post('/addoption', 'addOption');
+    });
 });
 
 Route::controller(UserController::class)->group(function () {
